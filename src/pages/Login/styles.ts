b26fb96ebeeee9f61be71f69d3532d2b9b8ee186
rubensojunior <rubensojunior6@gmail.com>
2020-05-01 18:21:08 -0300

    creating login page

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Input = styled.TextInput`
+  margin-top: 20px;
+  width: 90%;
+  background: #eee;
+  height: 40px;
+  border-width: 1px;
+  border-color: #eee;
+  font-family: 'Poppins-Regular';
+`
+
+export const Button = styled.TouchableOpacity`
+  margin-top: 30px;
+  padding: 10px;
+  background: #4286f4;
+`
+
+export const ButtonText = styled.Text`
+  font-family: 'Poppins-Regular';
+  font-size: 20px;
+  color: #fff;
+`
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-// import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
