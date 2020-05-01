01aa7cfe1dd78f74de7df532e009c9ff8abb43dc
rubensojunior <rubensojunior6@gmail.com>
2020-05-01 20:37:01 -0300

    adjusts in sign in page to improve UX

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,22 @@
 import React, { useState, useCallback } from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-import { Container, Title, ButtonText } from './styles'
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  CreateAccount,
+  ButtonText,
+} from './styles'
 
 import logoImg from '../../assets/logo.png'
 
@@ -17,36 +29,58 @@ const SignIn: React.FC = () => {
   }, [])
 
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Faça seu login</Title>
-
-      <Input
-        name="email"
-        icon="mail"
-        placeholder="E-mail"
-        autoFocus
-        keyboardType="email-address"
-        value={email}
-        onChangeText={(emailText) => setEmail(emailText)}
-      />
-      <Input
-        name="password"
-        icon="lock"
-        placeholder="Senha"
-        secureTextEntry
-        value={email}
-        onChangeText={(emailText) => setEmail(emailText)}
-      />
-      <Button
-        onPress={() => {
-          console.log('test')
-        }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
       >
-        Entrar
-      </Button>
-    </Container>
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Faça seu login</Title>
+          </View>
+
+          <Input
+            name="email"
+            icon="mail"
+            placeholder="E-mail"
+            autoFocus
+            keyboardType="email-address"
+            value={email}
+            onChangeText={(emailText) => setEmail(emailText)}
+          />
+
+          <Input
+            name="password"
+            icon="lock"
+            placeholder="Senha"
+            secureTextEntry
+            value={email}
+            onChangeText={(emailText) => setEmail(emailText)}
+          />
+
+          <Button
+            onPress={() => {
+              console.log('test')
+            }}
+          >
+            Entrar
+          </Button>
+
+          <ForgotPassword>
+            <ButtonText>Criar uma conta</ButtonText>
+          </ForgotPassword>
+
+          <CreateAccount>
+            <ButtonText>Esqueci minha senha</ButtonText>
+          </CreateAccount>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -8,30 +8,22 @@ export const Container = styled.View`
 `
 
 export const Title = styled.Text`
-  font-family: 'Poppins-Bold';
+  font-family: 'Poppins-Medium';
   font-size: 24px;
   color: #333333;
-  margin: 64px 0 24px;
+  margin: 44px 0 24px;
 `
 
-export const Input = styled.TextInput`
-  margin-top: 20px;
-  width: 90%;
-  background: #fff;
-  height: 40px;
-  border-width: 1px;
-  border-color: #eee;
-  font-family: 'Poppins-Regular';
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
 `
 
-export const Button = styled.TouchableOpacity`
-  margin-top: 30px;
-  padding: 10px;
-  background: #4286f4;
+export const CreateAccount = styled.TouchableOpacity`
+  margin-top: 10px;
 `
 
 export const ButtonText = styled.Text`
+  color: #333333;
+  font-size: 16px;
   font-family: 'Poppins-Regular';
-  font-size: 20px;
-  color: #fff;
 `
