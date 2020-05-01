f142753a1d050a37330517733492d1a33a783405
rubensojunior <rubensojunior6@gmail.com>
2020-05-01 18:43:24 -0300

    creating stack navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
+import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from '../pages/Feed'
 import AddPhoto from '../pages/AddPhoto'
 import Profile from '../pages/Profile'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 
 const Tab = createBottomTabNavigator()
+const Auth = createStackNavigator()
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#ff6b6b' },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+  </Auth.Navigator>
+)
 
 const Routes: React.FC = () => (
   <Tab.Navigator
@@ -29,14 +45,14 @@ const Routes: React.FC = () => (
       },
     })}
     tabBarOptions={{
-      activeTintColor: 'tomato',
+      activeTintColor: '#ff6b6b',
       inactiveTintColor: 'gray',
       showLabel: false,
     }}
   >
     <Tab.Screen name="Feed" component={Feed} />
     <Tab.Screen name="AddPhoto" component={AddPhoto} />
-    <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Screen name="Profile" component={AuthRoutes} />
   </Tab.Navigator>
 )
 
