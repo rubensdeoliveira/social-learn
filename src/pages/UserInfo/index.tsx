b13448cb962526a0647dda39900727962bba5e0c
rubensojunior <rubensojunior6@gmail.com>
2020-05-18 15:13:19 -0300

    implementing user info page

---
+++ src/pages/UserInfo/index.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { Back, BackText, Container, Image, Username, College } from './styles'
+import api from '../../services/api'
+
+const UserInfo: React.FC = () => {
+  const [usernameState, setUsernameState] = useState('')
+  const [college, setCollege] = useState('')
+  const [image, setImage] = useState('')
+
+  const navigation = useNavigation()
+  const route = useRoute()
+  const { userId } = route.params
+
+  useEffect(() => {
+    const loadUser = async (): Promise<void> => {
+      const response = await api.get('users.json')
+      const users = response.data
+      const usersKeys = Object.keys(users)
+
+      const findUserKey = usersKeys.find(
+        (userKey: string) => userKey === userId,
+      )
+
+      if (!findUserKey) return
+
+      const user = users[findUserKey]
+
+      if (user) {
+        setUsernameState(user.username)
+        setCollege(user.college)
+        setImage(user.image)
+      }
+    }
+
+    loadUser()
+  }, [])
+
+  return (
+    <Container>
+      {usernameState ? (
+        <View style={{ alignItems: 'center' }}>
+          {image ? <Image source={{ uri: image }} /> : null}
+          <Username>{usernameState}</Username>
+          <College>{college}</College>
+        </View>
+      ) : (
+        <Text>Usuário não existe ou foi alterado</Text>
+      )}
+
+      <Back
+        onPress={() => {
+          navigation.goBack()
+        }}
+      >
+        <BackText>voltar</BackText>
+      </Back>
+    </Container>
+  )
+}
+
+export default UserInfo
---
+++ src/pages/UserInfo/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  padding: 30px;
+`
+
+export const Back = styled.TouchableOpacity`
+  margin-top: auto;
+`
+
+export const BackText = styled.Text`
+  color: #333333;
+  font-size: 16px;
+  font-family: 'Poppins-Regular';
+`
+
+export const EmptyPhoto = styled.View`
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
+  margin-top: 60px;
+  background-color: #eee;
+`
+
+export const Image = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 75px;
+  margin-top: 60px;
+`
+
+export const Username = styled.Text`
+  margin-top: 30px;
+  font-size: 30px;
+  font-family: 'Poppins-Bold';
+  text-align: center;
+  color: #000;
+`
+
+export const College = styled.Text`
+  font-size: 25px;
+  font-family: 'Poppins-Regular';
+  text-align: center;
+  color: #000;
+`
